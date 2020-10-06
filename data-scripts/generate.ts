0e54361736ae6eb196eec9fe28fe87e75a7271ae
Hannes Rüger <hannesrueger@gmx.de>
2020-10-06 20:55:20 +0200

    added english lastnames

--- data-scripts/generate.ts
+++ data-scripts/generate.ts
@@ -2,9 +2,12 @@ import { GeneratorOptions, register, runGenerators } from "./_helpers/runtime";
 import { EnCommonWordsGenerator } from "./en/EnCommonWordsGenerator";
 import { DeCommonWordsGenerator } from "./de/DeCommonWordsGenerator";
 import { EnFirstnamesGenerator } from "./en/EnFirstnamesGenerator";
+import { EnLastnamesGenerator } from "./en/EnLastnamesGenerator";
 
 register("commonWords", "en", EnCommonWordsGenerator);
 register("firstnames", "en", EnFirstnamesGenerator);
+register("lastnames", "en", EnLastnamesGenerator);
+
 register("commonWords", "de", DeCommonWordsGenerator);
 
 runGenerators();
