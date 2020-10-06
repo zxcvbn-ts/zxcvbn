0dc85102455e19dfbdca064175f201c513d8d24a
Hannes Rüger <hannesrueger@gmx.de>
2020-10-06 20:51:23 +0200

    added lowercase option

--- data-scripts/_helpers/processors/simpleList.ts
+++ data-scripts/_helpers/processors/simpleList.ts
@@ -3,27 +3,28 @@ export function processSimpleList(list: string, options = {
     commentPrefixes: ["#", "//"],
     removeDuplicates: true,
     trimWhitespaces: true,
+    toLowerCase: false,
 }): string[] {
     let data = list.split(options.splitter);
-    console.log(data);
     if (Array.isArray(options.commentPrefixes)) {
         console.log("Filtering comments");
         for (const p of options.commentPrefixes) {
             data = data.filter((l) => !l.startsWith(p));
         }
     }
-    console.log(data);
     if (options.trimWhitespaces) {
         console.log("Filtering whitespaces");
         data = data.map((l) => l.trim());
     }
-    console.log(data);
     if (options.removeDuplicates) {
         console.log("Filtering duplicates");
         data = data.filter((item, pos) => {
             return data.indexOf(item) == pos;
         });
     }
-    console.log(data);
+    if (options.toLowerCase) {
+        console.log("Converting to lowercase");
+        data = data.map((l) => l.toLowerCase());
+    }
     return data;
 }
