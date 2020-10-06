b082cdd4de6a3c02602d7c9e729ef245fc6be051
Hannes Rüger <hannesrueger@gmx.de>
2020-10-06 20:55:07 +0200

    fixed default options

--- data-scripts/_helpers/processors/simpleList.ts
+++ data-scripts/_helpers/processors/simpleList.ts
@@ -1,28 +1,39 @@
-export function processSimpleList(list: string, options = {
+type Options = {
+    splitter: string,
+    commentPrefixes: string[],
+    removeDuplicates: boolean,
+    trimWhitespaces: boolean,
+    toLowerCase: boolean,
+};
+
+const defaultOptions: Options = {
     splitter: "\n",
     commentPrefixes: ["#", "//"],
     removeDuplicates: true,
     trimWhitespaces: true,
     toLowerCase: false,
-}): string[] {
-    let data = list.split(options.splitter);
-    if (Array.isArray(options.commentPrefixes)) {
+}
+
+export function processSimpleList(list: string, options?: Partial<Options>): string[] {
+    const effectiveOptions = Object.assign(defaultOptions, options);
+    let data = list.split(effectiveOptions.splitter);
+    if (Array.isArray(effectiveOptions.commentPrefixes)) {
         console.log("Filtering comments");
-        for (const p of options.commentPrefixes) {
+        for (const p of effectiveOptions.commentPrefixes) {
             data = data.filter((l) => !l.startsWith(p));
         }
     }
-    if (options.trimWhitespaces) {
+    if (effectiveOptions.trimWhitespaces) {
         console.log("Filtering whitespaces");
         data = data.map((l) => l.trim());
     }
-    if (options.removeDuplicates) {
+    if (effectiveOptions.removeDuplicates) {
         console.log("Filtering duplicates");
         data = data.filter((item, pos) => {
             return data.indexOf(item) == pos;
         });
     }
-    if (options.toLowerCase) {
+    if (effectiveOptions.toLowerCase) {
         console.log("Converting to lowercase");
         data = data.map((l) => l.toLowerCase());
     }
