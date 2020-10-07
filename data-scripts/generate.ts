335b1a648fffe67141b1769a3692f1fbf900f49d
Hannes Rüger <hannesrueger@gmx.de>
2020-10-07 18:11:05 +0200

    added german lastnames

--- data-scripts/generate.ts
+++ data-scripts/generate.ts
@@ -7,5 +7,6 @@ registerList("en", "lastnames", "https://raw.githubusercontent.com/arineng/arinc
 
 registerList("de", "commonWords", "http://pcai056.informatik.uni-leipzig.de/downloads/etc/legacy/Papers/top1000de.txt", SimpleListGenerator);
 registerList("de", "firstnames", "https://gist.githubusercontent.com/hrueger/2aa48086e9720ee9b87ec734889e1b15/raw", SimpleListGenerator);
+registerList("de", "lastnames", "https://gist.githubusercontent.com/hrueger/6599d1ac1e03b4c3dc432d722ffcefd0/raw", SimpleListGenerator);
 
 run();
