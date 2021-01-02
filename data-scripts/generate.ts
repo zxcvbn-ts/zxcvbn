066efa1663b7c55917971e61de779b3b5acc92e5
MrWook <mr.wook@web.de>
2021-01-02 11:22:38 +0100

    chore(project): uncomment everything

--- data-scripts/generate.ts
+++ data-scripts/generate.ts
@@ -4,44 +4,45 @@ import ListHandler from './_helpers/runtime'
 import { KeyboardAdjacencyGraph } from './_generators/KeyboardAdjacencyGraph'
 
 const lists = {
-  // en: {
-  //   commonWords: {
-  //     source:
-  //       'https://github.com/hermitdave/FrequencyWords/raw/master/content/2018/en/en_50k.txt',
-  //     options: { hasOccurrences: true },
-  //   },
-  //   firstnames: {
-  //     source:
-  //       'https://raw.githubusercontent.com/dominictarr/random-name/master/first-names.txt',
-  //   },
-  //   lastnames: {
-  //     source:
-  //       'https://raw.githubusercontent.com/arineng/arincli/master/lib/last-names.txt',
-  //   },
-  // },
-  // de: {
-  //   commonWords: {
-  //     source:
-  //       'https://raw.githubusercontent.com/hermitdave/FrequencyWords/master/content/2018/de/de_50k.txt',
-  //     options: { hasOccurrences: true },
-  //   },
-  //   firstnames: {
-  //     source:
-  //       'https://gist.githubusercontent.com/hrueger/2aa48086e9720ee9b87ec734889e1b15/raw',
-  //   },
-  //   lastnames: {
-  //     source:
-  //       'https://gist.githubusercontent.com/hrueger/6599d1ac1e03b4c3dc432d722ffcefd0/raw',
-  //   },
-  // },
+  en: {
+    commonWords: {
+      source:
+        'https://github.com/hermitdave/FrequencyWords/raw/master/content/2018/en/en_50k.txt',
+      options: { hasOccurrences: true },
+    },
+    firstnames: {
+      source:
+        'https://raw.githubusercontent.com/dominictarr/random-name/master/first-names.txt',
+    },
+    lastnames: {
+      source:
+        'https://raw.githubusercontent.com/arineng/arincli/master/lib/last-names.txt',
+    },
+  },
+  de: {
+    commonWords: {
+      source:
+        'https://raw.githubusercontent.com/hermitdave/FrequencyWords/master/content/2018/de/de_50k.txt',
+      options: { hasOccurrences: true },
+    },
+    firstnames: {
+      source:
+        'https://gist.githubusercontent.com/hrueger/2aa48086e9720ee9b87ec734889e1b15/raw',
+    },
+    lastnames: {
+      source:
+        'https://gist.githubusercontent.com/hrueger/6599d1ac1e03b4c3dc432d722ffcefd0/raw',
+    },
+  },
   common: {
     passwords: {
       generator: PasswordGenerator,
       customList: true,
     },
-    // keyboardLayouts: {
-    //   generator: KeyboardAdjacencyGraph,
-    // },
+    keyboardLayouts: {
+      generator: KeyboardAdjacencyGraph,
+      customList: true,
+    },
   },
 }
 
