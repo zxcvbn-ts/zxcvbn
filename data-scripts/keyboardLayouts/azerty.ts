f0a7382e9b4a140b97967c3c546f2217d64a5e14
MrWook <MrWook@users.noreply.github.com>
2021-01-13 07:36:43 +0100

    feat(keyboard): Add new AZERTY keyboard layout (#17)
    * feat(graphs): add azerty keyboard layout
    
    * fix(graphs): resolve qwertz and add missing <> keys
    
    * feat(graphs): newly generated and use prettier
    
    * test(keyboard): add test for new keyboardlayout azerty

---
+++ data-scripts/keyboardLayouts/azerty.ts
@@ -0,0 +1,10 @@
+export default {
+  slanted: true,
+  layout: `
+²~ &1 é2 "3 '4 (5 -6 è7 _8 ç9 à0 )° +=
+    aA zZ eE rR tT yY uU iI oO pP ^" $£
+     qQ sS dD fF gG hH jJ kK lL mM ù% *µ
+   <> wW xX cC vV bB nN ,? ;. :/ !§
+  `,
+  shiftedRx: /[1234567890°+AZERYTUIOP¨£QSDFGHJKLM%µ>WXCVBN?/.§]/,
+}
