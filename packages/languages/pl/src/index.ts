1fd62b77bc27b57d7968a6383a8404e2acc77683
Oskar Gmerek <oskarg90@gmail.com>
2022-04-12 20:59:44 +0100

    feat: add the polish dictionary and language package for zxcvbn-ts

---
+++ packages/languages/pl/src/index.ts
@@ -0,0 +1,19 @@
+import commonWords from './commonWords.json'
+import femaleFirstnames from './femaleFirstnames.json'
+import femaleLastnames from './femaleLastnames.json'
+import maleFirstnames from './maleFirstnames.json'
+import maleLastnames from './maleLastnames.json'
+import wikipedia from './wikipedia.json'
+import translations from './translations'
+
+export default {
+  dictionary: {
+    commonWords,
+    femaleFirstnames,
+    femaleLastnames,
+    maleFirstnames,
+    maleLastnames,
+    wikipedia,
+  },
+  translations,
+}
---
+++ packages/languages/pl/src/translations.ts
@@ -0,0 +1,63 @@
+export default {
+  warnings: {
+    straightRow:
+      'Występujące kolejno po sobie znaki na klawiaturze, są łatwe do odgadnięcia.',
+    keyPattern: 'Krótkie wzory klawiaturowe są łatwe do odgadnięcia.',
+    simpleRepeat:
+      'Powtarzające się znaki, takie jak „aaa”, są łatwe do odgadnięcia.',
+    extendedRepeat:
+      'Powtarzające się wzorce znaków, takie jak „abcabcabc”, są łatwe do odgadnięcia.',
+    sequences:
+      'Typowe sekwencje znaków, takie jak „abc”, są łatwe do odgadnięcia.',
+    recentYears: 'Ostatnie lata są łatwe do odgadnięcia',
+    dates: 'Daty są łatwe do odgadnięcia.',
+    topTen: 'To jest przesadnie często używane hasło.',
+    topHundred: 'To jest bardzo często używane hasło.',
+    common: 'To jest często używane hasło.',
+    similarToCommon: 'To jest podobne do często używanego hasła.',
+    wordByItself: 'Pojedyncze słowa są łatwe do odgadnięcia.',
+    namesByThemselves:
+      'Pojedyncze imiona lub nazwiska są łatwe do odgadnięcia.',
+    commonNames: 'Popularne imiona i nazwiska są łatwe do odgadnięcia.',
+    userInputs:
+      'Nie powinno być żadnych danych osobowych, ani danych dotyczących strony.',
+    pwned:
+      'To hasło zostało już ujawnione w wyniku naruszenia bezpieczeństwa danych w Internecie.',
+  },
+  suggestions: {
+    l33t: 'Unikaj przewidywalnych podstawień liter, takich jak „@” zamiast „a”.',
+    reverseWords: 'Unikaj pisowni popularnych słów na wspak.',
+    allUppercase:
+      'Niektóre litery napisz wielkimi literami, lecz nie wszystkie.',
+    capitalization: 'Używaj wielkiej litery, nie tylko na początku.',
+    dates: 'Unikaj dat i lat, które są z Tobą powiązane.',
+    recentYears: 'Unikaj ostatnich lat.',
+    associatedYears: 'Unikaj lat, które są z Tobą powiązane.',
+    sequences: 'Unikaj typowych sekwencji znaków.',
+    repeated: 'Unikaj powtarzających się słów i znaków.',
+    longerKeyboardPattern:
+      'Używaj dłuższych wzorów klawiaturowych i wielokrotnie zmieniaj kierunek pisania.',
+    anotherWord: 'Dodaj więcej słów, które są mniej popularne.',
+    useWords: 'Używaj wielu słów, ale unikaj popularnych fraz.',
+    noNeed:
+      'Możesz tworzyć silne hasła bez używania symboli, cyfr lub wielkich liter.',
+    pwned:
+      'Jeśli używasz tego hasła gdzie indziej, powinieneś je zmienić jak najszybciej.',
+  },
+  timeEstimation: {
+    ltSecond: 'mniej niż sekunda',
+    second: '{base} sekunda',
+    seconds: '{base} sekund',
+    minute: '{base} minuta',
+    minutes: '{base} minut',
+    hour: '{base} godzina',
+    hours: '{base} godzin',
+    day: '{base} dzień',
+    days: '{base} dni',
+    month: '{base} miesiąc',
+    months: '{base} miesięcy',
+    year: '{base} rok',
+    years: '{base} lat',
+    centuries: 'wieki',
+  },
+}
