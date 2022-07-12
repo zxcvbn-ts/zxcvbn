bd77ffe29918bad04c049408b26133af524351f6
MrWook <mr.wook@web.de>
2022-07-12 15:33:09 +0200

    feat(ja-language): add improved wikipedia dictionary

--- packages/languages/ja/src/index.ts
+++ packages/languages/ja/src/index.ts
@@ -1,6 +1,7 @@
 import commonWords from './commonWords.json'
 import firstnames from './firstnames.json'
 import lastnames from './lastnames.json'
+import wikipedia from './wikipedia.json'
 import translations from './translations'
 
 export default {
@@ -8,6 +9,7 @@ export default {
     commonWords,
     firstnames,
     lastnames,
+    wikipedia,
   },
   translations,
 }
