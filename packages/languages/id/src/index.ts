4e634f3e6351a94ce674f8789e641f15f3d481d6
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-09-06 14:40:05 +0700

    fix(languages): use names.json instead

--- packages/languages/id/src/index.ts
+++ packages/languages/id/src/index.ts
@@ -1,14 +1,12 @@
 import commonWords from './commonWords.json'
-import firstnames from './firstnames.json'
-import lastnames from './lastnames.json'
+import names from './names.json'
 import wikipedia from './wikipedia.json'
 import translations from './translations'
 
 export default {
   dictionary: {
     commonWords,
-    firstnames,
-    lastnames,
+    names,
     wikipedia,
   },
   translations,
