6b5f90c6070a41fe0fc0db79176a2f946c125a72
MrWook <mr.wook@web.de>
2023-02-01 15:39:47 +0100

    fix(debounce): third parameter is not required

--- packages/libraries/main/src/debounce.ts
+++ packages/libraries/main/src/debounce.ts
@@ -2,11 +2,14 @@ export type Procedure = (...args: any[]) => void
 
 /**
  * @link https://davidwalsh.name/javascript-debounce-function
+ * @param func needs to implement a function which is debounced
+ * @param wait how long do you want to wait till the previous declared function is executed
+ * @param isImmediate defines if you want to execute the function on the first execution or the last execution inside the time window. `true` for first and `false` for last.
  */
 export default <F extends Procedure>(
   func: F,
   wait: number,
-  isImmediate: boolean,
+  isImmediate?: boolean,
 ): ((this: ThisParameterType<F>, ...args: Parameters<F>) => void) => {
   let timeout: ReturnType<typeof setTimeout> | undefined
   return function debounce(this: ThisParameterType<F>, ...args: Parameters<F>) {
