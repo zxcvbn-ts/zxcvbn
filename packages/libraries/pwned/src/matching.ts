568f8935249f5f4f5e6e09f207dd5f4f171cc1fb
MrWook <mr.wook@web.de>
2023-02-10 15:33:49 +0100

    feat!(pwned): add options to handle network errors

--- packages/libraries/pwned/src/matching.ts
+++ packages/libraries/pwned/src/matching.ts
@@ -1,18 +1,25 @@
 // @ts-ignore
 import { MatchExtended, MatchOptions } from '@zxcvbn-ts/core'
 import haveIBeenPwned from './haveIBeenPwned'
-import { FetchApi } from './types'
+import { FetchApi, MatcherPwnedFactoryConfig } from './types'
 
 /*
  * -------------------------------------------------------------------------------
  *  Have i been pwned matching factory ---------------------------------------------------
  * -------------------------------------------------------------------------------
  */
-export default (universalFetch: FetchApi, url?: string) => {
+export default (
+  universalFetch: FetchApi,
+  { url, networkErrorHandler }: MatcherPwnedFactoryConfig,
+) => {
   return class MatchPwned {
     async match({ password }: MatchOptions) {
       const matches: MatchExtended[] = []
-      const pwned = await haveIBeenPwned(password, universalFetch, url)
+      const pwned = await haveIBeenPwned(password, {
+        universalFetch,
+        url,
+        networkErrorHandler,
+      })
       if (pwned) {
         // @ts-ignore
         matches.push({
--- packages/libraries/pwned/src/types.ts
+++ packages/libraries/pwned/src/types.ts
@@ -1,1 +1,13 @@
 export type FetchApi = WindowOrWorkerGlobalScope['fetch']
+
+export type NetworkErrorHandler = (error: Error | Response) => false
+
+export interface MatcherPwnedFactoryConfig {
+  url?: string
+  networkErrorHandler?: NetworkErrorHandler
+}
+export interface HaveIBeenPwnedConfig {
+  universalFetch: FetchApi
+  url?: string
+  networkErrorHandler?: NetworkErrorHandler
+}
