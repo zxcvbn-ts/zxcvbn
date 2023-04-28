4df8a4e76826f1c15c4f45081ea3ef3121e23476
zwerner <drummerkyran77@gmail.com>
2023-04-27 21:15:07 -0400

    Fix issue with bruteforce matching and separators

--- packages/libraries/main/src/scoring/index.ts
+++ packages/libraries/main/src/scoring/index.ts
@@ -80,14 +80,20 @@ const scoringHelper = {
   bruteforceUpdate(passwordCharIndex: number) {
     // see if a single bruteforce match spanning the passwordCharIndex-prefix is optimal.
     let match = this.makeBruteforceMatch(0, passwordCharIndex)
-    if (!HAS_SEPARATOR.test(match.token)) this.update(match, 1)
+    const separatorMatches = [...match.token.matchAll(HAS_SEPARATOR)]
+    let i = 1
+    if (separatorMatches.length) {
+      let match = separatorMatches[separatorMatches.length - 1].index ?? 1
+      i = match + 1
+    } else {
+      this.update(match, 1)
+    }
 
-    for (let i = 1; i <= passwordCharIndex; i += 1) {
+    for (; i <= passwordCharIndex; i += 1) {
       // generate passwordCharIndex bruteforce matches, spanning from (i=1, j=passwordCharIndex) up to (i=passwordCharIndex, j=passwordCharIndex).
       // see if adding these new matches to any of the sequences in optimal[i-1]
       // leads to new bests.
       match = this.makeBruteforceMatch(i, passwordCharIndex)
-      if (HAS_SEPARATOR.test(match.token)) return
       const tmp = this.optimal.m[i - 1]
       // eslint-disable-next-line no-loop-func
       Object.keys(tmp).forEach((sequenceLength) => {
