852a9065cf18b488c9128a92aad0226f7ec0a5b5
MrWook <mr.wook@web.de>
2023-05-12 14:38:16 +0200

    fix(common): resolve issue with wrong type generation

--- packages/languages/common/src/index.ts
+++ packages/languages/common/src/index.ts
@@ -1,7 +1,8 @@
-import adjacencyGraphs from './adjacencyGraphs.json'
+import * as tempAdjacencyGraphs from './adjacencyGraphs.json'
 import passwords from './passwords.json'
 import diceware from './diceware.json'
 
 const dictionary = { passwords, diceware }
-
+// reinit the variable because the typescript compile will otherwise just reexport the json file which will break as it's a json file
+const adjacencyGraphs = tempAdjacencyGraphs
 export { dictionary, adjacencyGraphs }
