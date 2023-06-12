3b251e9409e6e3e8de8c095bf93651e61a921412
MrWook <mr.wook@web.de>
2023-06-12 17:48:17 +0200

    refactor(testing): use object notation for so many arguments

--- packages/libraries/main/test/helper/checkMatches.ts
+++ packages/libraries/main/test/helper/checkMatches.ts
@@ -1,51 +1,65 @@
 import { LooseObject, MatchExtended } from '../../src/types'
 
 // eslint-disable-next-line jest/no-export
-export default (
-  prefix: string,
-  matches: MatchExtended[],
-  patternNames: string,
-  patterns: string[],
-  ijs: number[][],
-  props: LooseObject,
-) => {
+export interface CheckMatchesOptions {
+  messagePrefix: string
+  matches: MatchExtended[]
+  patternNames: string
+  patterns: string[]
+  ijs: number[][]
+  propsToCheck: LooseObject
+}
+
+// eslint-disable-next-line jest/no-export
+export default function checkMatches({
+  messagePrefix,
+  matches,
+  patternNames,
+  patterns,
+  ijs,
+  propsToCheck,
+}: CheckMatchesOptions) {
   const usedPatternNames = patterns.map(() => patternNames)
   let isEqualLenArgs =
     usedPatternNames.length === patterns.length &&
     patterns.length === ijs.length
 
-  Object.keys(props).forEach((prop) => {
-    const lst = props[prop]
+  Object.keys(propsToCheck).forEach((prop) => {
+    const lst = propsToCheck[prop]
     isEqualLenArgs = isEqualLenArgs && lst.length === patterns.length
   })
+
   if (!isEqualLenArgs) {
     throw Error('unequal argument lists to check_matches')
   }
-  it(`${prefix}: matches.length == ${patterns.length}`, () => {
+
+  it(`${messagePrefix}: matches.length == ${patterns.length}`, () => {
     expect(matches.length).toEqual(patterns.length)
   })
+
   for (let k = 0; k < patterns.length; k += 1) {
     const match = matches[k]
     const patternName = usedPatternNames[k]
     const pattern = patterns[k]
     const [i, j] = ijs[k]
-    it(`${prefix}: matches[${k}].pattern == '${usedPatternNames}'`, () => {
+
+    it(`${messagePrefix}: matches[${k}].pattern == '${usedPatternNames}'`, () => {
       expect(match.pattern).toEqual(patternName)
     })
-    it(`${prefix}: matches[${k}] should have [i, j] of [${i}, ${j}]`, () => {
+    it(`${messagePrefix}: matches[${k}] should have [i, j] of [${i}, ${j}]`, () => {
       expect([match.i, match.j]).toEqual([i, j])
     })
-    it(`${prefix}: matches[${k}].token == '${pattern}'`, () => {
+    it(`${messagePrefix}: matches[${k}].token == '${pattern}'`, () => {
       expect(match.token).toEqual(pattern)
     })
 
-    Object.keys(props).forEach((propName) => {
-      const propList = props[propName]
+    Object.keys(propsToCheck).forEach((propName) => {
+      const propList = propsToCheck[propName]
       let propMsg = propList[k]
       if (typeof propMsg === 'string') {
         propMsg = `'${propMsg}'`
       }
-      it(`${prefix}: matches[${k}].${propName} == ${propMsg}`, () => {
+      it(`${messagePrefix}: matches[${k}].${propName} == ${propMsg}`, () => {
         // @ts-ignore
         expect(match[propName]).toEqual(propList[k])
       })
--- packages/libraries/main/test/matcher/regex/matching.spec.ts
+++ packages/libraries/main/test/matcher/regex/matching.spec.ts
@@ -3,16 +3,33 @@ import checkMatches from '../../helper/checkMatches'
 
 describe('regex matching', () => {
   const data = [
-    ['1922', 'recentYear'],
-    ['2017', 'recentYear'],
+    { pattern: '1922', regexNames: ['recentYear'], ijs: [[0, 3]] },
+    { pattern: '2017', regexNames: ['recentYear'], ijs: [[0, 3]] },
+    {
+      pattern: '2016-2015',
+      regexNames: ['recentYear', 'recentYear'],
+      ijs: [
+        [0, 3],
+        [5, 8],
+      ],
+    },
   ]
 
   const matchRegex = new MatchRegex()
-  data.forEach(([pattern, name]) => {
+  data.forEach(({ pattern, regexNames, ijs }) => {
     const matches = matchRegex.match({ password: pattern })
-    const msg = `matches ${pattern} as a ${name} pattern`
-    checkMatches(msg, matches, 'regex', [pattern], [[0, pattern.length - 1]], {
-      regexName: [name],
+    const msg = `matches ${pattern} as a ${regexNames[0]} pattern`
+    const patterns = pattern.split('-')
+
+    checkMatches({
+      messagePrefix: msg,
+      matches,
+      patternNames: 'regex',
+      patterns,
+      ijs,
+      propsToCheck: {
+        regexName: regexNames,
+      },
     })
   })
 })
