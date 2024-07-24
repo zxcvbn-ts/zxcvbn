592daae51d810158d9ab72189a313026f6cb22ef
Danut Florian <dan@attacksimulator.com>
2024-07-24 17:25:15 +0300

    Added the romanian language

---
+++ packages/languages/ro/src/translations.ts
@@ -0,0 +1,52 @@
+export default {
+    warnings: {
+        straightRow: 'Tastele consecutive de pe tastatură sunt ușor de ghicit.',
+        keyPattern: 'Parolele scurte sunt ușor de ghicit.',
+        simpleRepeat: 'Caracterele repetate, cum ar fi "aaa", sunt ușor de ghicit.',
+        extendedRepeat: 'Modele repetate precum "abcabcabc" sunt ușor de ghicit.',
+        sequences: 'Secvențele de caractere comune precum "abc" sunt ușor de ghicit.',
+        recentYears: 'Anii recenți sunt ușor de ghicit.',
+        dates: 'Datele sunt ușor de ghicit.',
+        topTen: 'Este una dintre cele mai frecvent utilizate parole.',
+        topHundred: 'Este o parolă utilizată frecvent.',
+        common: 'Este o parolă folosită frecvent.',
+        similarToCommon: 'Este similară cu o parolă folosită frecvent.',
+        wordByItself: 'Cuvintele simple sunt ușor de ghicit.',
+        namesByThemselves: 'Numele și prenumele singure sunt ușor de ghicit.',
+        commonNames: 'Numele și prenumele comune sunt ușor de ghicit.',
+        userInputs: 'Pe această pagină nu ar trebui să existe date personale sau conexe.',
+        pwned: 'Parola ta a fost expusă de o încălcare a securității datelor pe internet.'
+    },
+    suggestions: {
+        l33t: "Evită substituțiile previzibile, cum ar fi \"@\" pentru \"@\".",
+        reverseWords: 'Evită cuvintele comune scrise invers',
+        allUppercase: 'Scrie cu majuscule unele litere, dar nu toate literele.',
+        capitalization: 'Scrie cu majusculă unele litere în plus față de prima literă.',
+        dates: 'Evită datele care îți sunt asociate.',
+        recentYears: 'Evită anii recenți.',
+        associatedYears: 'Evită anii asociați cu dumneavoastră.',
+        sequences: 'Evită secvențele comune de litere.',
+        repeated: 'Evită cuvintele și literele care se repetă.',
+        longerKeyboardPattern: 'Utilizați modele de tastatură mai lungi și schimbă direcția de tastare de mai multe ori.',
+        anotherWord: 'Adaugă mai multe cuvinte care sunt mai puțin comune.',
+        useWords: 'Folosește mai multe cuvinte, dar evită frazele comune.',
+        noNeed: 'Poți crea parole puternice fără să folosești simboluri, numere sau majuscule.',
+        pwned: 'Dacă folosești această parolă în altă parte, ar trebui să o schimbi.'
+    },
+    timeEstimation: {
+        ltSecond: 'mai puțin de o secundă',
+        second: '{base} secundă',
+        seconds: '{base} secunde',
+        minute: '{base} minut',
+        minutes: '{base} minute',
+        hour: '{base} oră',
+        hours: '{base} hours',
+        day: '{base} day',
+        days: '{base} days',
+        month: '{base} month',
+        months: '{base} luni',
+        year: '{base} an',
+        years: '{base} ani',
+        centuries: 'secole'
+    }
+}
