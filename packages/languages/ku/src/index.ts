ca6263ba3131fdc118302e7aeae26e199e7ca7de
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2024-11-20 19:03:10 +0000

    Kurdish (Kurmanjî dialect) Language support

---
+++ packages/languages/ku/src/index.ts
@@ -0,0 +1,14 @@
+import firstnames from './firstnames.json'
+import lastnames from './lastnames.json'
+import commonWords from './commonWords.json'
+import translations from './translations'
+import wikipedia from './wikipedia.json'
+
+const dictionary = {
+  'commonWords-ku': commonWords,
+  'firstNames-ku': firstnames,
+  'lastNames-ku': lastnames,
+  'wikipedia-ku': wikipedia,
+}
+
+export { dictionary, translations }
