2360267dc24055b4f387a2ccd41f21f9a0f469e3
MrWook <mr.wook@web.de>
2026-08-05 20:12:31 +0200

    chore: debug linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,7 @@ export default defineConfig([
     '**/coverage',
     'data-scripts/wikiExtractor/extracts',
     '**/dist_new',
+    '**/*.json',
   ]),
   js.configs.recommended,
   compatPlugin.configs['flat/recommended'],
